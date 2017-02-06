719a6eb46a8afcdee4e03a3e7f8cbcc9c5c7666d
Ben Jervis <ben@jervis.net.au>
2017-02-06 21:31:42 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,5 +22,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  
+  test 'should get contact' do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
+  
+  test 'root should point to home' do
+    get root_url
+    assert_response :success
+    assert_select 'title', "Home | #{@base_title}"
+  end
 
 end
