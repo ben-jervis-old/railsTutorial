139bc01b0fe789e46b98c1587cdb6fc75e081679
Ben Jervis <ben@jervis.net.au>
2017-02-08 02:35:03 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     
     before_save { email.downcase! }
+    
     has_secure_password
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)+[a-z]\z/i
